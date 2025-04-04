ef8f562bded03fe369caa3888b047cbbfdf09ff6
michalowsik <michal.owsik95@gmail.com>
2025-04-05 00:36:55 +0200

    Przywrócono poprzedni projekt do repozytorium

---
+++ Type_your_name/2ndProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FirstProject
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Type in your name");
+            
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello");
+            Console.WriteLine(name);
+        }
+    }
+}
