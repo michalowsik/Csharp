5b17799b3f64e0f0a28da23f19823c7ac04cfc2c
michalowsik <michal.owsik95@gmail.com>
2025-05-08 02:22:04 +0200

    Dodano Wybory do repozytorium

---
+++ Wybory/Wybory/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Komentarze_regiony
+{
+    #region 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ile masz lat?");
+            string userInput = Console.ReadLine();
+
+            int age = int.Parse(userInput);
+            bool User18plus = age >= 18;
+            if (User18plus)
+            {
+                Console.WriteLine("Możesz głosować");
+            }
+
+            else
+            {
+                Console.WriteLine("Nie jesteś jeszcze uprawniony do głosowania!");
+            }
+            Console.WriteLine("Bye bye");
+        }
+
+    }
+    #endregion
+
+}
